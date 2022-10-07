8ee5c19b1d128a170b4fa74773d95784a3c88f9e
leo.lu <luqizheng@126.com>
2022-10-07 10:21:42 +0800

    文件类型

--- Coder.File2Object/OtherColumns/ExcelOtherColumnSetting.cs
+++ Coder.File2Object/OtherColumns/ExcelOtherColumnSetting.cs
@@ -1,20 +1,35 @@
-﻿using NPOI.SS.UserModel;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
+using NPOI.SS.UserModel;
+
 namespace Coder.File2Object.OtherColumns
 {
+    /// <summary>
+    /// </summary>
+    /// <typeparam name="TEntity"></typeparam>
+    /// <typeparam name="TReturnValue"></typeparam>
     public class ExcelOtherColumnSetting<TEntity, TReturnValue> :
         OtherColumnSetting<TEntity, ICell, TReturnValue>
     {
         private readonly Func<string, TReturnValue> _convertToValue;
 
+        /// <summary>
+        /// </summary>
+        /// <param name="action"></param>
+        /// <param name="convertToValue"></param>
         public ExcelOtherColumnSetting(Expression<Func<TEntity, Dictionary<string, TReturnValue>>> action, Func<string, TReturnValue> convertToValue)
             : base(action)
         {
             _convertToValue = convertToValue;
         }
 
+        /// <summary>
+        /// </summary>
+        /// <param name="cell"></param>
+        /// <param name="val"></param>
+        /// <param name="errorMessage"></param>
+        /// <returns></returns>
         protected override bool TryConvert(ICell cell, out TReturnValue val, out string errorMessage)
         {
             errorMessage = null;
@@ -23,19 +38,28 @@ namespace Coder.File2Object.OtherColumns
             val = _convertToValue(strValue);
             return true;
         }
-
     }
 
+    /// <summary>
+    /// </summary>
+    /// <typeparam name="TEntity"></typeparam>
     public class ExcelOtherColumnSetting<TEntity> :
         OtherColumnSetting<TEntity, ICell, string>
     {
-
+        /// <summary>
+        /// </summary>
+        /// <param name="action"></param>
         public ExcelOtherColumnSetting(Expression<Func<TEntity, Dictionary<string, string>>> action)
             : base(action)
         {
-
         }
 
+        /// <summary>
+        /// </summary>
+        /// <param name="cell"></param>
+        /// <param name="val"></param>
+        /// <param name="errorMessage"></param>
+        /// <returns></returns>
         protected override bool TryConvert(ICell cell, out string val, out string errorMessage)
         {
             errorMessage = null;
@@ -43,6 +67,5 @@ namespace Coder.File2Object.OtherColumns
             val = cell.StringCellValue?.Trim();
             return true;
         }
-
     }
 }
